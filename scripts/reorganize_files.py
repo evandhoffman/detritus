ac8a0482a6b3b95bb2f62061a24a27e6aea0a49a
Evan D. Hoffman <evandhoffman@gmail.com>
2016-06-13 00:16:27 -0400

    Actuall do the move

--- scripts/reorganize_files.py
+++ scripts/reorganize_files.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python
 
-import os, os.path, time, glob
+import os, os.path, time, hashlib
 
 src_path = "/Users/evan/testdir"
 tgt_path = "/Users/evan/dstdir"
-extensions = ['jpg', 'txt']
+extensions = ['.mov', '.mp4']
 path_format = "%Y/%Y-%m/%Y-%m-%d"
 filename_prefix_format = "%Y-%m-%d"
 
-def move_files( file_list):
+def move_files( file_list, dry_run=True):
   for f in file_list:
     new_path = get_new_filename(os.path.normpath(f))
     print("Moving %s to %s" % (f, new_path))
+    if (not dry_run):
+      dir_name = os.path.dirname(new_path)
+      if (not os.path.exists(dir_name)):
+        os.makedirs(dir_name)
+      os.rename(f, new_path)
 
 def get_file_list( src_path, recursive=True ):
   dir_list = os.listdir( src_path )
@@ -20,7 +25,7 @@ def get_file_list( src_path, recursive=True ):
     full_path = "/".join([src_path, f])
     if (os.path.isfile(full_path)):
       for e in extensions:
-        if (full_path.endswith(e)):
+        if (full_path.lower().endswith(e)):
           file_list.append(full_path)
     else:
       if (os.path.isdir(full_path) and recursive):
@@ -30,19 +35,21 @@ def get_file_list( src_path, recursive=True ):
 
 
 
-def get_new_filename( src_filename ):
+def get_new_filename( src_filename , with_digest=True):
   file_ctime = time.localtime(os.path.getctime(src_filename))
+  digest=''
+  if (with_digest):
+    digest = hashlib.md5(open(src_filename, 'rb').read()).hexdigest()
+
   new_prefix = time.strftime(filename_prefix_format, file_ctime)
   new_path = "/".join(
       [tgt_path, 
         time.strftime(path_format, file_ctime), 
         ".".join(
-          [new_prefix, 
+          [new_prefix, digest[:8],
             os.path.basename(src_filename)])])
   return new_path
 
 
 #f = get_file_list(src_path)
-f = move_files(get_file_list(os.path.abspath(src_path)))
-
-print f
+f = move_files(get_file_list(os.path.abspath(src_path)), dry_run=False)
